ab795171b40a01eaa9adad5b7d49d50d160e0732
sagiters19 <sagiters19@gmail.com>
2019-05-09 00:14:48 +0800

    Init & Complete card view with UIPanGesture

---
+++ SwipeMatchFirestoreLBTA/Views/CardView.swift
@@ -0,0 +1,79 @@
+//
+//  CardView.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Jack on 2019/5/8.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    
+    // Configurations
+    fileprivate let threshold: CGFloat = 80
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // custom drawing code
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded(gesture)
+        default:
+            ()
+        }
+        
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        // rotation
+        // some not that scary math here to convert radians to degrees
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
+        
+    }
+    
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        
+        let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+        let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            if shouldDismissCard {
+                
+                self.frame = CGRect(x: 1000 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+                
+            } else {
+                self.transform = .identity
+            }
+        }) { (_) in
+            self.transform = .identity
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ SwipeMatchFirestoreLBTA/Views/HomeBottomControlsStackView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeBottomControlsStackView.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Jack on 2019/5/8.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import UIKit
+
+class HomeBottomControlsStackView: UIStackView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        distribution = .fillEqually
+        heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let subviews = [#imageLiteral(resourceName: "refresh_circle"), #imageLiteral(resourceName: "dismiss_circle"), #imageLiteral(resourceName: "super_like_circle"), #imageLiteral(resourceName: "like_circle"), #imageLiteral(resourceName: "boost_circle")].map { (img) -> UIView in
+            let button = UIButton(type: .system)
+            button.setImage(img.withRenderingMode(.alwaysOriginal), for: .normal)
+            return button
+        }
+        
+        subviews.forEach { (v) in
+            addArrangedSubview(v)
+        }
+        
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
