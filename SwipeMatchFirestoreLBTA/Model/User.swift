19cea0732548050133f455f4955a6ad28a3217af
sagiters19 <sagiters19@gmail.com>
2019-05-09 01:11:29 +0800

    Refactor user model and card view with cardViewModel

--- SwipeMatchFirestoreLBTA/Model/User.swift
+++ SwipeMatchFirestoreLBTA/Model/User.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Jack. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct User {
     // defining our properties for our model layer
@@ -15,6 +15,14 @@ struct User {
     let profession: String
     let imageName: String
     
+    func toCardViewModel() -> CardViewModel {
+        
+        let attributedText = NSMutableAttributedString(string: name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
+        attributedText.append(NSMutableAttributedString(string: "  \(age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
+        attributedText.append(NSMutableAttributedString(string: "\n\(profession)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
+        
+        return CardViewModel(imageName: imageName, attributedString: attributedText, textAlignment: .left)
+    }
+    
 }
-
 
---
+++ SwipeMatchFirestoreLBTA/ViewModels/CardViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  CardViewModel.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Jack on 2019/5/9.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    // we'll define the properties that are view will display/render out
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
+
+// What  exactly do we do with this card view model thing???
+
