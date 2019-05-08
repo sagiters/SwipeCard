096185311d6de6959f292083a4a8cc43dfffa8d9
sagiters19 <sagiters19@gmail.com>
2019-05-09 00:52:42 +0800

    Create user model & add label to card view

---
+++ SwipeMatchFirestoreLBTA/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Jack on 2019/5/9.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    // defining our properties for our model layer
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+    
+}
+
+
--- SwipeMatchFirestoreLBTA/Views/CardView.swift
+++ SwipeMatchFirestoreLBTA/Views/CardView.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class CardView: UIView {
     
-    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
     
     // Configurations
     fileprivate let threshold: CGFloat = 80
+    let infomationLabel = UILabel()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,8 +22,17 @@ class CardView: UIView {
         layer.cornerRadius = 10
         clipsToBounds = true
         
+        imageView.contentMode = .scaleAspectFill
         addSubview(imageView)
         imageView.fillSuperview()
+        
+        addSubview(infomationLabel)
+        infomationLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
+        
+        infomationLabel.text = "TEST NAME TEST NAME AGE"
+        infomationLabel.textColor = .white
+        infomationLabel.font = UIFont.systemFont(ofSize: 34, weight: .heavy)
+        infomationLabel.numberOfLines = 0
         
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
         addGestureRecognizer(panGesture)
@@ -58,17 +68,21 @@ class CardView: UIView {
         let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
         let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
         
-        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
             if shouldDismissCard {
                 
-                self.frame = CGRect(x: 1000 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+                self.frame = CGRect(x: 600 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
                 
             } else {
                 self.transform = .identity
             }
         }) { (_) in
             self.transform = .identity
-            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+            if shouldDismissCard {
+                self.removeFromSuperview()
+            }
+            
+//            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
         }
     }
     
