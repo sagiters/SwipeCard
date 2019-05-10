7104db8bfed138885752ba744483c6dfebc4ff80
sagiters19 <sagiters19@gmail.com>
2019-05-11 02:00:45 +0800

    Cycling Through User Photos

--- SwipeMatchFirestoreLBTA/Controllers/HomeController.swift
+++ SwipeMatchFirestoreLBTA/Controllers/HomeController.swift
@@ -14,17 +14,12 @@ class HomeController: UIViewController {
     let cardsDeckView = UIView()
     let buttonsStackView = HomeBottomControlsStackView()
     
-//    let users = [
-//        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c"),
-//        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c")
-//    ]
-    
     let cardViewModels: [CardViewModel] = {
         let producers = [
-            User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c"),
-            User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c"),
+            User(name: "Kelly", age: 23, profession: "Music DJ", imageNames: ["kelly1", "kelly2", "kelly3"]),
+            User(name: "Jane", age: 18, profession: "Teacher", imageNames: ["lady4c"]),
             Advertiser(title: "Slide Out Menu", brandName: "Lets Build That App", posterPhotoName: "slide_out_menu_poster"),
-            User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c"),
+            User(name: "Jane", age: 18, profession: "Teacher", imageNames: ["jane1", "jane2", "jane3"]),
         ] as [ProducesCardViewModel]
         
         let viewModels = producers.map({ return $0.toCardViewModel() })
