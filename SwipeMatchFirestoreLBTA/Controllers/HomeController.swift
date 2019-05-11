5da4824e61b46b8c0eb3cd05c0f5341b4ffaa65a
sagiters19 <sagiters19@gmail.com>
2019-05-12 01:15:52 +0800

    Creating a Registration Page

--- SwipeMatchFirestoreLBTA/Controllers/HomeController.swift
+++ SwipeMatchFirestoreLBTA/Controllers/HomeController.swift
@@ -29,10 +29,18 @@ class HomeController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        topStackView.settingsButton.addTarget(self, action: #selector(handleSettings), for: .touchUpInside)
+        
         setupLayout()
         setupDummyCards()
         
 
+    }
+    
+    @objc func handleSettings() {
+        print("Show registration page")
+        let registrationController = RegistrationViewController()
+        present(registrationController, animated: true, completion: nil)
     }
     
     fileprivate func setupDummyCards() {
---
+++ SwipeMatchFirestoreLBTA/Views/CustomTextField.swift
@@ -0,0 +1,36 @@
+//
+//  CustomTextField.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Jack on 2019/5/12.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    let padding: CGFloat
+    
+    init(padding: CGFloat) {
+        self.padding = padding
+        super.init(frame: .zero)
+        layer.cornerRadius = 25
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: 50)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
