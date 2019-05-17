afc22b5f6cddc307284436f312cd1a0555accc2e
sagiters19 <sagiters19@gmail.com>
2019-05-17 14:07:16 +0800

    Detect Matches and Omit Swiped Cards

--- SwipeMatchFirestoreLBTA/Controllers/HomeController.swift
+++ SwipeMatchFirestoreLBTA/Controllers/HomeController.swift
@@ -79,6 +79,26 @@ class HomeController: UIViewController, SettingsControllerDelegate, LoginControl
                 return
             }
             self.user = user
+            
+            self.fetchSwipes()
+            
+//            self.fetchUsersFromFirestore()
+        }
+    }
+    
+    var swipes = [String: Int]()
+    
+    fileprivate func fetchSwipes() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Firestore.firestore().collection("swipes").document(uid).getDocument { (snapshot, err) in
+            if let err = err {
+                print("failed to fetch swipes info for currently logged in user:", err)
+                return
+            }
+            
+            print("Swipes:", snapshot?.data() ?? "")
+            guard let data = snapshot?.data() as? [String: Int] else { return }
+            self.swipes = data
             self.fetchUsersFromFirestore()
         }
     }
@@ -118,7 +138,9 @@ class HomeController: UIViewController, SettingsControllerDelegate, LoginControl
             snapshot?.documents.forEach({ (documentSnapshot) in
                 let userDictionary = documentSnapshot.data()
                 let user = User(dictionary: userDictionary)
-                if user.uid != Auth.auth().currentUser?.uid {
+                let isNotCurrentUser = user.uid != Auth.auth().currentUser?.uid
+                let hasNotSwipedBefore = self.swipes[user.uid!] == nil
+                if isNotCurrentUser && hasNotSwipedBefore {
                     let cardView = self.setupCardFromUser(user: user)
                     
                     previousCardView?.nextCardView = cardView
@@ -171,6 +193,7 @@ class HomeController: UIViewController, SettingsControllerDelegate, LoginControl
                     }
                     
                     print("Successfully updated swiped...")
+                    self.checkIfMatchExists(cardUID: cardUID)
                 }
             } else {
                 Firestore.firestore().collection("swipes").document(uid).setData(documentData) { (err) in
@@ -180,6 +203,7 @@ class HomeController: UIViewController, SettingsControllerDelegate, LoginControl
                     }
                     
                     print("Successfully save swiped...")
+                    self.checkIfMatchExists(cardUID: cardUID)
                 }
             }
         }
@@ -187,6 +211,32 @@ class HomeController: UIViewController, SettingsControllerDelegate, LoginControl
         
         
         
+    }
+    
+    fileprivate func checkIfMatchExists(cardUID: String) {
+        // Howto detect our match between two users?
+        print("Detecting match")
+        
+        Firestore.firestore().collection("swipes").document(cardUID).getDocument { (snapshot, err) in
+            if let err = err {
+                print("Failed to fetch document for card user:", err)
+                return
+            }
+            
+            guard let data = snapshot?.data() else { return }
+            print(data)
+            
+            guard let uid = Auth.auth().currentUser?.uid else { return }
+            
+            let hasMatched = data[uid] as? Int == 1
+            if hasMatched {
+                print("Has matched")
+                let hud = JGProgressHUD(style: .dark)
+                hud.textLabel.text = "Found a match"
+                hud.show(in: self.view)
+                hud.dismiss(afterDelay: 4)
+            }
+        }
     }
     
     @objc func handleDislike() {
